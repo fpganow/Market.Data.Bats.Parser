ea65b61e6454f1860e8271c16e10860837e12a0f
Fpga John <john@fpganow.com>
2024-01-10 17:53:41 -0500

    Adding another attribute to func

--- ip_export/bats_loader.py
+++ ip_export/bats_loader.py
@@ -19,7 +19,7 @@ class MyList:
     def get_idx(self, idx):
         return self._data[idx]
 
-    @sv
+    @sv()
     def set_idx(self, idx, value):
         if len(self._data) > idx:
             self._data[idx] = value
@@ -32,8 +32,24 @@ class MyList:
     def get_length(self):
         return len(self._data)
 
+    @sv()
     def from_bytearray(self, in_byte_array):
         self._data = in_byte_array
+
+
+def test_get_time():
+    from hamcrest import assert_that, equal_to
+    # GIVEN
+    time_in_s = 34_200
+    my_list = MyList();
+    my_list.append(100)
+
+    # WHEN
+    get_time(34200, my_list);
+
+    # THEN
+    assert_that(my_list.get_length(), equal_to(6))
+
 
 @sv(sec_since_midnight=DataType.Int,
     out_list=MyList,
@@ -61,8 +77,12 @@ def get_time(sec_since_midnight: int, out_list: MyList) -> int:
 
     return 0
 
+def compile():
+    # compile the a shared_lib into build folder
+    lib_path = compile_lib([MyList, get_time], cwd="build")
 
-# compile the a shared_lib into build folder
-lib_path = compile_lib([MyList, get_time], cwd="build")
-# generate SV binding
-generate_sv_binding([MyList, get_time], filename="pysv_pkg.sv")
+    # generate SV binding
+    generate_sv_binding([MyList, get_time], filename="pysv_pkg.sv")
+
+if __name__ == "__main__":
+    compile()
